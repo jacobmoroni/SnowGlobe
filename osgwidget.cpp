8f60ba987cb501c0b025653be54dd5804cafdb4e
Corey McBride <corey.mcbride@byu.edu>
2018-09-25 21:00:28 -0600

    Initial commit

---
+++ osgwidget.cpp
@@ -0,0 +1,248 @@
+#include "OSGWidget.h"
+
+#include <osg/Camera>
+#include <osg/DisplaySettings>
+#include <osg/Geode>
+#include <osg/Material>
+#include <osg/Shape>
+#include <osg/ShapeDrawable>
+#include <osg/StateSet>
+#include <osgDB/WriteFile>
+#include <osgGA/EventQueue>
+#include <osgViewer/View>
+#include <osgViewer/ViewerEventHandlers>
+#include <osg/MatrixTransform>
+#include <osg/NodeVisitor>
+#include <osg/LineWidth>
+#include <osgUtil/SmoothingVisitor>
+
+#include <osgParticle/FireEffect>
+
+#include <cassert>
+#include <vector>
+
+#include <QKeyEvent>
+#include <QPainter>
+#include <QWheelEvent>
+
+OSGWidget::OSGWidget( QWidget* parent, Qt::WindowFlags flags ):
+    QOpenGLWidget{ parent,flags },
+    mGraphicsWindow{ new osgViewer::GraphicsWindowEmbedded{ this->x(),
+                                                            this->y(),
+                                                            this->width(),
+                                                            this->height() } }
+  , mViewer{ new osgViewer::CompositeViewer }
+{
+    mRoot = new osg::Group;
+
+    osgViewer::View* mView = new osgViewer::View;
+
+    float aspectRatio = static_cast<float>( this->width() ) / static_cast<float>( this->height() );
+    auto pixelRatio   = this->devicePixelRatio();
+
+    osg::Camera* camera = new osg::Camera;
+    camera->setViewport( 0, 0, this->width() * pixelRatio, this->height() * pixelRatio );
+
+    camera->setClearColor( osg::Vec4( 0.f, 0.f, .5, 1.f ) );
+    camera->setProjectionMatrixAsPerspective( 45.f, aspectRatio, 1.f, 1000.f );
+    camera->setGraphicsContext( mGraphicsWindow );
+
+    mView->setCamera( camera );
+    mView->setSceneData( mRoot.get() );
+    mView->addEventHandler( new osgViewer::StatsHandler );
+
+    osg::ref_ptr<osgGA::TrackballManipulator> manipulator = new osgGA::TrackballManipulator;
+    manipulator->setAllowThrow( false );
+    manipulator->setHomePosition(osg::Vec3d(0.0,-20.0,3.0),osg::Vec3d(0,0,0),osg::Vec3d(0,0,1));
+
+    mView->setCameraManipulator( manipulator );
+    mViewer->addView( mView );
+    mViewer->setThreadingModel( osgViewer::CompositeViewer::SingleThreaded );
+    mViewer->realize();
+    mView->home();
+
+    osg::Sphere* sphere    = new osg::Sphere( osg::Vec3( 0.f, 0.f, 0.f ), 2.0f );
+    osg::ShapeDrawable* sd = new osg::ShapeDrawable( sphere );
+    sd->setColor( osg::Vec4( 1.f, 0.f, 0.f, 1.f ) );
+    sd->setName( "Sphere" );
+
+    osg::Geode* geode = new osg::Geode;
+    geode->addDrawable( sd );
+
+    osg::StateSet* stateSet = geode->getOrCreateStateSet();
+    osg::Material* material = new osg::Material;
+
+    material->setColorMode( osg::Material::AMBIENT_AND_DIFFUSE );
+
+    stateSet->setAttributeAndModes( material, osg::StateAttribute::ON );
+    stateSet->setMode( GL_DEPTH_TEST, osg::StateAttribute::ON );
+
+    mRoot->addChild(geode);
+
+    this->setFocusPolicy( Qt::StrongFocus );
+    this->setMinimumSize( 100, 100 );
+    this->setMouseTracking( true );
+
+    this->update();
+}
+
+OSGWidget::~OSGWidget()
+{
+}
+
+void OSGWidget::paintEvent( QPaintEvent* /* paintEvent */ )
+{
+    this->makeCurrent();
+
+    QPainter painter( this );
+    painter.setRenderHint( QPainter::Antialiasing );
+
+    this->paintGL();
+
+    painter.end();
+
+    this->doneCurrent();
+}
+
+void OSGWidget::paintGL()
+{
+    mViewer->frame();
+}
+
+void OSGWidget::resizeGL( int width, int height )
+{
+    this->getEventQueue()->windowResize( this->x(), this->y(), width, height );
+    mGraphicsWindow->resized( this->x(), this->y(), width, height );
+
+    this->on_resize( width, height );
+}
+
+void OSGWidget::keyPressEvent( QKeyEvent* event )
+{
+    QString keyString   = event->text();
+    const char* keyData = keyString.toLocal8Bit().data();
+
+    if( event->key() == Qt::Key_H )
+    {
+        mView->home();
+        return;
+    }
+
+    this->getEventQueue()->keyPress( osgGA::GUIEventAdapter::KeySymbol( *keyData ) );
+}
+
+void OSGWidget::keyReleaseEvent( QKeyEvent* event )
+{
+    QString keyString   = event->text();
+    const char* keyData = keyString.toLocal8Bit().data();
+
+    this->getEventQueue()->keyRelease( osgGA::GUIEventAdapter::KeySymbol( *keyData ) );
+}
+
+void OSGWidget::mouseMoveEvent( QMouseEvent* event )
+{
+
+    auto pixelRatio = this->devicePixelRatio();
+
+    this->getEventQueue()->mouseMotion( static_cast<float>( event->x() * pixelRatio ),
+                                        static_cast<float>( event->y() * pixelRatio ) );
+}
+
+void OSGWidget::mousePressEvent( QMouseEvent* event )
+{
+    // 1 = left mouse button
+    // 2 = middle mouse button
+    // 3 = right mouse button
+
+    unsigned int button = 0;
+
+    switch( event->button() )
+    {
+    case Qt::LeftButton:
+        button = 1;
+        break;
+
+    case Qt::MiddleButton:
+        button = 2;
+        break;
+
+    case Qt::RightButton:
+        button = 3;
+        break;
+
+    default:
+        break;
+    }
+
+    auto pixelRatio = this->devicePixelRatio();
+
+    this->getEventQueue()->mouseButtonPress( static_cast<float>( event->x() * pixelRatio ),
+                                             static_cast<float>( event->y() * pixelRatio ),
+                                             button );
+
+}
+
+void OSGWidget::mouseReleaseEvent(QMouseEvent* event)
+{
+    // 1 = left mouse button
+    // 2 = middle mouse button
+    // 3 = right mouse button
+
+    unsigned int button = 0;
+
+    switch( event->button() )
+    {
+    case Qt::LeftButton:
+        button = 1;
+        break;
+
+    case Qt::MiddleButton:
+        button = 2;
+        break;
+
+    case Qt::RightButton:
+        button = 3;
+        break;
+
+    default:
+        break;
+    }
+
+    auto pixelRatio = this->devicePixelRatio();
+
+    this->getEventQueue()->mouseButtonRelease( static_cast<float>( pixelRatio * event->x() ),
+                                               static_cast<float>( pixelRatio * event->y() ),
+                                               button );
+}
+
+void OSGWidget::wheelEvent( QWheelEvent* event )
+{
+    event->accept();
+    int delta = event->delta();
+
+    osgGA::GUIEventAdapter::ScrollingMotion motion = delta > 0 ?   osgGA::GUIEventAdapter::SCROLL_UP
+                                                                 : osgGA::GUIEventAdapter::SCROLL_DOWN;
+
+    this->getEventQueue()->mouseScroll( motion );
+}
+
+void OSGWidget::on_resize( int width, int height )
+{
+    std::vector<osg::Camera*> cameras;
+    mViewer->getCameras( cameras );
+
+    auto pixelRatio = this->devicePixelRatio();
+
+    cameras[0]->setViewport( 0, 0, width * pixelRatio, height * pixelRatio );
+}
+
+osgGA::EventQueue* OSGWidget::getEventQueue() const
+{
+    osgGA::EventQueue* eventQueue = mGraphicsWindow->getEventQueue();
+
+    if( eventQueue )
+        return eventQueue;
+    else
+        throw std::runtime_error( "Unable to obtain valid event queue");
+}
+
